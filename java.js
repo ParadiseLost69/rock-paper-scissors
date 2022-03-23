ceedb9ae56c2ffedf501783d68bc5c9485d6cd1f
Teddy <tedloukas@outlook.com>
2022-03-23 15:05:09 +0900

    added computerPlay function java.js

--- java.js
+++ java.js
@@ -1,1 +1,38 @@
+let teddy = "hello"
+console.log(teddy)
 
+function gradeCalc (gradeOne, gradeTwo, gradeThree, gradeFour) {
+    finalGrade = (gradeOne + gradeTwo + gradeThree + gradeFour) / 4 // retrieve average
+    if (finalGrade < 50) {
+        console.log ("Your grade is " + finalGrade + " YOU ARE FAILING")  //indicate failing grade
+    } else {
+        console.log ("Your grade is " + finalGrade + " Keep trying your best!") //indicate passing grade
+    }
+
+    
+    console.log (finalGrade)
+}
+
+//randomly returns ROCK PAPER or SCISSORS
+// set var R P S
+//use random to select
+//output selection
+//make random number - if 0 display rock, if 1 display paper, if 2 display scissors
+
+
+
+
+let x = Math.floor(Math.random() * 3) // values of 0,1,2
+console.log(x) 
+
+function computerPlay() {
+    selection = Math.floor(Math.random() * 3)
+    if (selection === 0){
+        console.log("rock")
+    } else if (selection === 1) {
+        console.log("paper")
+    } else {
+        console.log("scissors")
+    }
+}
+function rockPaperScissors (playerSelection, computerSelection)
